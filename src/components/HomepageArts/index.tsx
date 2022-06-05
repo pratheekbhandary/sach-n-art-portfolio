f6fde204e50b2664fd2f251ff2a42e14a541f54b
pratheekbhandary <pratheekbhandary@github.com>
2022-06-05 20:23:05 +0100

    [feat] add pills for type

--- src/components/HomepageArts/index.tsx
+++ src/components/HomepageArts/index.tsx
@@ -6,30 +6,58 @@ import _artifactJson from "../../content/artifacts.json";
 import { ArtifactsType } from "../../content/artifacts.model";
 import { Fade } from "react-awesome-reveal";
 import { Link } from "react-router-dom";
-import { StyledImageListItem } from "./styles";
+import { Pill, PillContainer, StyledImageListItem } from "./styles";
 import { useMediaQuery } from "@mui/material";
 
 const artifacts = _artifactJson as ArtifactsType;
 
 export default function HomepageArts() {
   const matches = useMediaQuery("(min-width:600px)");
+  const uniqueTypes = React.useMemo(
+    () => Array.from(new Set(artifacts.projects.map((p) => p.type))),
+    []
+  );
+  const allType = "All";
+  const [selectedType, setSelectedType] = React.useState<string>(allType);
+
+  const selectedProject = React.useMemo(
+    () =>
+      selectedType === allType
+        ? artifacts.projects
+        : artifacts.projects.filter((p) => p.type === selectedType),
+    [selectedType]
+  );
   return (
-    <ImageList cols={matches ? 2 : 1}>
-      {artifacts.projects.map((item, index) => (
-        <Fade>
-          <Link to={`/arts/${index}`}>
-            <StyledImageListItem key={item.name}>
-              <img
-                src={item.thumbnail}
-                srcSet={item.thumbnail}
-                alt={item.name}
-                loading="lazy"
-              />
-              <ImageListItemBar title={item.name} />
-            </StyledImageListItem>
-          </Link>
-        </Fade>
-      ))}
-    </ImageList>
+    <>
+      <PillContainer>
+        {[allType, ...uniqueTypes].map((type) => (
+          <Pill
+            $selected={selectedType === type}
+            onClick={() => {
+              setSelectedType(type);
+            }}
+          >
+            {type}
+          </Pill>
+        ))}
+      </PillContainer>
+      <ImageList cols={matches ? 2 : 1}>
+        {selectedProject.map((item, index) => (
+          <Fade>
+            <Link to={`/arts/${index}`}>
+              <StyledImageListItem key={item.name}>
+                <img
+                  src={item.thumbnail}
+                  srcSet={item.thumbnail}
+                  alt={item.name}
+                  loading="lazy"
+                />
+                <ImageListItemBar title={item.name} />
+              </StyledImageListItem>
+            </Link>
+          </Fade>
+        ))}
+      </ImageList>
+    </>
   );
 }
--- src/components/HomepageArts/styles.ts
+++ src/components/HomepageArts/styles.ts
@@ -15,3 +15,22 @@ export const StyledImageListItem = styled(ImageListItem)`
   }
   overflow: hidden;
 `;
+
+export const PillContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  padding: 20px;
+  flex-wrap: wrap;
+`;
+
+export const Pill = styled.div<{ $selected?: boolean }>`
+  background-color: ${({ $selected }) => {
+    return $selected ? "rgb(255, 130, 92)" : "#2E186A";
+  }};
+  color: ${({ $selected }) => (!$selected ? "rgb(255, 130, 92)" : "#2E186A")};
+  padding: 5px 20px;
+  border-radius: 25px;
+  margin: 10px;
+  cursor: pointer;
+  font-weight: bold;
+`;
