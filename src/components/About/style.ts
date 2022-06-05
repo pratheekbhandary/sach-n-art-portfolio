20da651d351e752748e0b00456a43298392c39a2
pratheekbhandary <pratheekbhandary@github.com>
2022-06-05 21:28:51 +0100

    add about page, svg color, ...

--- src/components/About/style.ts
+++ src/components/About/style.ts
@@ -3,13 +3,14 @@ import styled from "styled-components";
 export const AboutDescription = styled.div`
   display: flex;
   justify-content: center;
-  align-items: center;
+  align-items: flex-start;
 
   > * {
     margin: 30px;
   }
   img {
-    width: 50%;
+    display: inline-block;
+    width: 25%;
     object-fit: contain;
     border-radius: 100%;
     border: 10px solid rgb(255, 130, 92);
@@ -17,6 +18,8 @@ export const AboutDescription = styled.div`
 
   @media screen and (max-width: 768px) {
     flex-direction: column;
-    width: 100%;
+    img {
+      width: 100%;
+    }
   }
 `;
